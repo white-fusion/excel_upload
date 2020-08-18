00f953071f07be4bf7c49edfa7442b51e1019d35
white-fusion <r9yash@gmail.com>
2020-08-18 08:28:59 +0530

    Fix errors while converting the data

--- src/components/UploadButton.vue
+++ src/components/UploadButton.vue
@@ -32,26 +32,28 @@ export default {
     },
     convertData(obj, structure){
       //assume all keys in obj have lower case by now
-      //assume mandatory check has been done by now
-      //Dont miss the case where that field is not mandatory and theres no data about it
-      var converted = structure;
-      for(const dummy in converted){
-        if(converted[dummy].meta.column){
+      //Mandatory checks should be done before calling this function
+      console.log(obj);
+      Object.keys(structure).forEach(dummy => {
+        console.log(dummy);
+        if(structure[dummy].meta.column){
           if(Object.keys(obj).includes(dummy.toLowerCase())){
-            //type check
-            //var typeArray = converted[dummy].meta.type;
-            //if (!typeArray.includes(typeof(obj["dummy.toLowerCase"])))
-            //{return alert;}
-            converted[dummy] = obj["dummy.toLowerCase"];
-          } else{
-            converted[dummy] = null;
+            // var typeArray = structure[dummy].meta.type;
+            // if (!typeArray.includes(typeof(obj[dummy.toLowerCase()]))){
+            //   return alert;
+            // }
+            structure[dummy] = obj[dummy.toLowerCase()];
+            console.log('Is a column');
+            console.log(structure[dummy]);
+          } else{ //If it is not mandatory and data is not there
+            //structure[dummy] = null;
           }
-        } else{ //if it's not a column
-          this.convertData(obj, converted[dummy]);
-          delete converted[dummy].meta;
-          delete converted[dummy].fields;
+        } else if(!structure[dummy].meta.column){//If it is not a column
+          console.log('aint a column');
+          structure[dummy] = structure[dummy].fields; //deletes meta and fields and directly bring up all subfields as an object
+          this.convertData(obj, structure[dummy]);
         }
-      }
+      });
     },
     importExcel(e) {
       const files = e.target.files;
@@ -74,22 +76,48 @@ export default {
           const lowHeaders = headers.map(header => header.toLowerCase());
           console.log(lowHeaders);
 
-          this.getMandFields(this.dataStruct); 
-          
+          this.getMandFields(this.dataStruct);  
           const lowMand = this.mand.map(e => e.toLowerCase());
           console.log(lowMand);
           // lowMand.forEach(function(entry){
           //   if(!lowHeaders.includes(entry)){              
-          //     return alert("Mandaatory field " + entry + " not present in the file!");//after alert it shouldn't continue functioning anymore
+          //     return alert("Mandatory field " + entry + " not present in the file!");//after alert it shouldn't continue functioning anymore
           //   } 
           // });
 
+          
+          
+          
+          
+          
+          var structure = this.dataStruct;
+          var key, keys = Object.keys(ws[1]);
+          var n = keys.length;
+          var obj={}
+          while (n--) {
+            key = keys[n];
+            obj[key.toLowerCase()] = ws[1][key];
+          }
+          this.convertData(obj, structure);
+          console.log(structure);
+
+
+
+
+
+
+
+
+
           const excellist = []; //To edit received data
           for (var j = 0; j < ws.length; j++) {
-            excellist.push(ws[j]);
+            //var structure = this.dataStruct;
+            //Do I need to return an object or is it dynamic
             //Checking type
+            //this.convertData(ws[j], structure);
 
             //Here convert it to the required datastructure
+            excellist.push(ws[j]);
           }
           console.log("Read results", excellist); // an array containing objects that need to be processed
         } catch (e) {
