e8fe872e5a6146b2392a30954c2a7f61f93c3c4a
Yaswanth <yaswanth@linarc.io>
2020-10-16 16:33:02 +0530

    package configurations

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+// import app from '../test/sample.vue'
+import UploadButton from './components/UploadButton.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(app),
+// }).$mount('#app')
+export default UploadButton
---
+++ test/sample.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <UploadButton @onSubmit="submit" v-bind:dataStruct="dataStruct" v-bind:titleText="titleText"/>
+  </div>
+</template>
+
+<script>
+import UploadButton from '@/components/UploadButton.vue'
+
+export default {
+  name: 'sample',
+  methods:{
+    submit(obj){
+      console.log(obj);
+    }
+  },
+  data() {
+    return {
+      titleText: "UPLOAD",
+      dataStruct: {
+        companyName: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: true
+          }
+        },
+        businessType: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        },
+        contactName: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        },
+        title: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        },
+        email: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        },
+        country: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        },
+        phone: {
+          meta: {
+            hasSubfields: false,
+            type: ['string'],
+            mandatory: false
+          }
+        }
+      }  
+    }
+  },
+  components: {
+    UploadButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
